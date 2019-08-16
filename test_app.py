5a5378c70e3c66663430823233956e1708884382
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-08-16 11:13:05 +0200

    Remove “praise” tests
    I removed the corresponding non-test code in 188054ee6f already.

--- test_app.py
+++ test_app.py
@@ -1,5 +1,4 @@
 import pytest  # type: ignore
-import re
 
 import app as pagepile_visual_filter
 
@@ -30,37 +29,3 @@ def test_csrf_token_load():
     with pagepile_visual_filter.app.test_request_context() as context:
         context.session['csrf_token'] = 'test token'
         assert pagepile_visual_filter.csrf_token() == 'test token'
-
-
-def test_praise(client):
-    # default praise
-    response = client.get('/praise')
-    html = response.get_data(as_text=True)
-    assert '<h2>You rock!</h2>' in html
-
-    # extract CSRF token
-    match = re.search(r'name="csrf_token" type="hidden" value="([^"]*)"', html)
-    assert match is not None
-    csrf_token = match.group(1)
-
-    referrer = pagepile_visual_filter.full_url('praise')
-    headers = {'Referer': referrer}
-
-    # update praise
-    response = client.post('/praise',
-                           data={'csrf_token': csrf_token,
-                                 'praise': 'How cool!'},
-                           headers=headers)
-    html = response.get_data(as_text=True)
-    assert '<h2>How cool!</h2>' in html
-    assert 'You rock!' not in html
-
-    # try to update praise with expired CSRF token
-    response = client.post('/praise',
-                           data={'csrf_token': csrf_token,
-                                 'praise': 'Boo!'},
-                           headers=headers)
-    html = response.get_data(as_text=True)
-    assert '<h2>Boo!</h2>' not in html
-    assert '<h2>How cool!</h2>' in html
-    assert 'value="Boo!"' in html  # input is repeated
