93add9ed8bf16f89c8c8210ed3fcba969f0ad93b
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-07-20 21:28:10 +0200

    Update config loading code
    Combining two cookiecuttor-toolforge commits [1][2].
    
    [1]: https://github.com/lucaswerkmeister/cookiecutter-toolforge/commit/6efec9b6a08e2278279d11d2fd6bc9de13159e33
    [2]: https://github.com/lucaswerkmeister/cookiecutter-toolforge/commit/17ef149635e37676c944486c49d2f9a2cd46ef6f

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import decorator
 import flask
 import mwapi  # type: ignore
 import os
 import random
+import stat
 import string
 import toolforge
+from typing import Any, Callable
 import yaml
 
 from pagepile import load_pagepile, create_pagepile
@@ -17,11 +20,29 @@ user_agent = toolforge.set_user_agent(
     'pagepile-visual-filter',
     email='mail@lucaswerkmeister.de')
 
-__dir__ = os.path.dirname(__file__)
-try:
-    with open(os.path.join(__dir__, 'config.yaml')) as config_file:
-        app.config.update(yaml.safe_load(config_file))
-except FileNotFoundError:
+
+@decorator.decorator
+def read_private(func: Callable, *args: Any, **kwargs: Any) -> Any:
+    try:
+        f = args[0]
+        fd = f.fileno()
+    except AttributeError:
+        pass
+    except IndexError:
+        pass
+    else:
+        mode = os.stat(fd).st_mode
+        if (stat.S_IRGRP | stat.S_IROTH) & mode:
+            name = getattr(f, "name", "config file")
+            raise ValueError(f'{name} is readable to others, '
+                             'must be exclusively user-readable!')
+    return func(*args, **kwargs)
+
+
+has_config = app.config.from_file('config.yaml',
+                                  load=read_private(yaml.safe_load),
+                                  silent=True)
+if not has_config:
     print('config.yaml file not found, assuming local development setup')
     characters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(characters) for _ in range(64))
